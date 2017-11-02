9beceafc1887856816603bb7e0cace7707a618e4
Mike <michael.ries@gmail.com>
2017-11-01 20:38:56 -0500

    Done.

--- tests/ContactController.spec.js
+++ tests/ContactController.spec.js
@@ -1,31 +1,32 @@
-describe('ContactController', function () {
-	var $controller;
+describe("ContactController", function() {
+  var $controller;
 
-	beforeEach(module('app'));
+  beforeEach(module("app"));
 
-	beforeEach(inject(function (_$controller_) {
-		$controller = _$controller_;
-	}));
+  beforeEach(
+    inject(function(_$controller_) {
+      $controller = _$controller_;
+    })
+  );
 
+  it('should have a name property set to "Bill Gates"', function() {
+    var $scope = {};
+    $controller("ContactController", { $scope: $scope });
 
-	it('should have a name property set to "Bill Gates"', function () {
-		var $scope = {};
-		$controller('ContactController', {$scope: $scope});
+    expect($scope.name).toBe("Bill Gates");
+  });
 
-		expect($scope.name).toBe('Steve Jobs');
-	});
+  it('should have a title set to "Founder"', function() {
+    var $scope = {};
+    $controller("ContactController", { $scope: $scope });
 
-	it('should have a title set to "Founder"', function () {
-		var $scope = {};
-		$controller('ContactController', {$scope: $scope});
+    expect($scope.title).toBe("Founder");
+  });
 
-		expect($scope.title).toBe('Cleaner');
-	});
+  it('should have an email set to "bill@microsoft.com"', function() {
+    var $scope = {};
+    $controller("ContactController", { $scope: $scope });
 
-	it('should have an email set to "bill@microsoft.com"', function () {
-		var $scope = {};
-		$controller('ContactController', {$scope: $scope});
-
-		expect($scope.email).toBe('tim@apple.com');
-	});
+    expect($scope.email).toBe("bill@microsoft.com");
+  });
 });
